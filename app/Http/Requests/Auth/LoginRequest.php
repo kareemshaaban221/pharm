c433309eebfe273e3e29861785d68cf59e5ce819
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-12-25 18:36:04 +0200

    test\

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -28,4 +28,18 @@ class LoginRequest extends BaseRequest
             'password' => ['required', Password::min(8)->numbers()->letters()->mixedCase()->symbols()],
         ];
     }
+
+    public function messages(): array {
+        return [
+            'required' => '.هذا الحقل مطلوب',
+            'email.email' => 'هذا الحقل يجب ان يكون بريد الكتروني صحيح.',
+            'password' => [
+                'min' => 'هذا الحقل (كلمة المرور) يجب الا يقل عن 8 حروف.',
+                'numbers' => 'هذا الحقل (كلمة المرور) يجب ان يحتوي على ارقام.',
+                'letters' => 'هذا الحقل (كلمة المرور) يجب ان يحتوي على حروف.',
+                'mixedCase' => 'هذا الحقل (كلمة المرور) يجب ان يحتوي على حرف كبير و حرف صغير.',
+                'symbols' => 'هذا الحقل (كلمة المرور) يجب ان يحتوي على رموز.',
+            ],
+        ];
+    }
 }
